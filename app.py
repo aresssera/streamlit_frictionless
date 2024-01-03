3eda572a56d38d72b0c805f90df87eb97f44b8be
Jana <123444692+aresssera@users.noreply.github.com>
2024-01-03 09:07:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def perform_quality_check(file):
                             # perform validation using schema matched to uploaded file
                             report = validate(file, schema=schema)
                             
-                            return report
+                            return "hallo"
 
                         return f"No schema found for the uploaded file '{file_name}' in the datapackage."
 
